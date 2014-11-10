944861000b1270dfc0a4eee4c698abb1f267539c
Liam Costello <liam@transifex.com>
2014-11-10 17:09:35 +0200

    Base strategy is now an instance method.

--- hermes/strategies.py
+++ hermes/strategies.py
@@ -3,10 +3,12 @@ class ErrorStrategy(object):
     Abstract strategy for handling errors returned from components
     """
 
-    @classmethod
-    def handle_exception(cls, error):
+    def handle_exception(self, error):
         """
         An abstract method that must be overidden by subclasses.
+
+        Must return a tuple of:
+        (Boolean indicating if the exception was handled, a string message)
         """
         raise NotImplementedError("Subclasses MUST override the "
                                   "'handle_exception' method")
